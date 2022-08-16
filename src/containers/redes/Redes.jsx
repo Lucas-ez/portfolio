c6643c14e3904cbeb3fb396207a065656203dcdd
Lucas-ez <lucasezequiel237@gmail.com>
2022-08-16 14:52:05 -0300

    Add resposive

--- src/containers/redes/Redes.jsx
+++ src/containers/redes/Redes.jsx
@@ -2,11 +2,31 @@ import React from 'react'
 import './redes.css'
 
 import { Title } from '../../components'
+import { HiMail } from 'react-icons/hi'
+import { FaGithub } from 'react-icons/fa'
+import { BsTelegram, BsLinkedin, BsInstagram } from 'react-icons/bs'
 
 function Redes() {
   return (
     <div className='Redes' id='redes'>
         <Title text={"Redes"}/>
+        <div className='Redes_container'>
+          <a href="">
+            <HiMail className='Redes_icon'/>
+          </a>
+          <a href="">
+            <FaGithub className='Redes_icon'/>
+          </a>
+          <a href="">
+            <BsLinkedin className='Redes_icon'/>
+          </a>
+          <a href="">
+            <BsTelegram className='Redes_icon'/>
+          </a>
+          <a href="">
+            <BsInstagram className='Redes_icon'/>
+          </a>
+        </div>
     </div>
   )
 }
