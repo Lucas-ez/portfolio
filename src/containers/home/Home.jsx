6f1b9c0b00f37435be8213cd191ad1dd184a9844
Lucas-ez <lucasezequiel237@gmail.com>
2022-09-01 15:50:06 -0300

    Add Descripcion en home

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
       <div className='home_text-section'>
         <div className='home_title'>Lucas Brest</div>
         <div className='home_subtitle'>{"< Desarrollador Web FullStack />"}</div>
-        <div className='home_description'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi voluptatum est amet facilis, veritatis sit quas neque delectus nulla non ab fugit consequuntur dolorem laborum esse laboriosam repellat. Reiciendis, recusandae.</div>
+        <div className='home_description'>Desarrollador web fullstack con capacidad de trabajar en el diseño y desarrollo de aplicaciones web y en la implementación de bases de datos relacionales. Orientado al uso de tecnologías web como HTML, CSS, JavaScript, React, Node.js y Bases de datos SQL. Con experiencia en el uso del software de control de versiones Git.</div>
       </div>
       <div className='home_img-section'>
         <img src={portrait} alt='portrait'/>
